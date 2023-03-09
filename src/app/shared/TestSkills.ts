22a984619ca4dc6cd47247efba037a87508b8508
Sumit Kukreja <sumit.kukreja@live.com>
2023-03-09 17:29:00 -0500

    what was $scope again?

---
+++ src/app/shared/TestSkills.ts
@@ -0,0 +1,12 @@
+import { Skill } from "./Skill"
+
+export const SKILLS: Skill[] = [
+    {
+        id: 1,
+        skillName: "Fixing",
+    },
+    {
+        id: 2,
+        skillName: "Flying",
+    },
+]
