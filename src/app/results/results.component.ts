4475b295960d94831992a7fb193e598cf2047960
Sumit Kukreja <sumit.kukreja@live.com>
2023-03-12 15:27:12 -0400

    mongodb connection

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,36 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { JobsService } from '../services/jobs.service';
+import { Job } from '../models/job.model';
 
 @Component({
   selector: 'app-results',
   templateUrl: './results.component.html',
   styleUrls: ['./results.component.sass']
 })
-export class ResultsComponent {
-  searchResults: { id: number, jobTitle: string, skillName: string, jobImage: string, jobDescription: string, jobCategory: string, jobPostingDate: string }[] = [
-  new Listing(0, "Test1", "Carpentry", "https://picsum.photos/200", "This is test job 1", "test", "3/9/2023"),
-  new Listing(1, "Test2", "Carpentry", "https://picsum.photos/200", "This is test job 2", "test", "3/9/2023"),
-  new Listing(2, "Test3", "Carpentry", "https://picsum.photos/200", "This is test job 3", "test", "3/8/2023"),
-  new Listing(3, "Test4", "Carpentry", "https://picsum.photos/200", "This is test job 4", "test", "3/8/2023"),
-  new Listing(4, "Test5", "Carpentry", "https://picsum.photos/200", "This is test job 5", "test", "3/7/2023"),
-  new Listing(5, "Test6", "Carpentry", "https://picsum.photos/200", "This is test job 6", "test", "3/6/2023"),
-  new Listing(6, "Test7", "Carpentry", "https://picsum.photos/200", "This is test job 7", "test", "3/5/2023"),
-  ]
+export class ResultsComponent implements OnInit {
+  postsPerPage = 2;
+  currentPage = 1;
+  searchResults: { 
+    _id: number, 
+    jobTitle: string, 
+    skillName: string, 
+    jobImage: string, 
+    jobDescription: string, 
+    jobCategory: string, 
+    jobPostingDate: string, 
+    jobAddressStreet: string,
+    jobAddressCity:string,
+    jobAddressCountry:string, 
+    jobAddressPostal:string }[] = [
+      {"_id":0, "jobTitle": "Test1", "skillName": "Carpentry", "jobImage": "https://picsum.photos/200", "jobDescription": "This is test job 1", "jobCategory": "test", "jobPostingDate": "3/9/2023", "jobAddressStreet": "", "jobAddressCity": "", "jobAddressCountry": "", "jobAddressPostal": ""},
+      {"_id":1, "jobTitle": "Test2", "skillName": "Carpentry", "jobImage": "https://picsum.photos/200", "jobDescription": "This is test job 2", "jobCategory": "test", "jobPostingDate": "3/9/2023", "jobAddressStreet": "", "jobAddressCity": "", "jobAddressCountry": "", "jobAddressPostal": ""},
+      {"_id":2, "jobTitle": "Test3", "skillName": "Carpentry", "jobImage": "https://picsum.photos/200", "jobDescription": "This is test job 3", "jobCategory": "test", "jobPostingDate": "3/8/2023", "jobAddressStreet": "", "jobAddressCity": "", "jobAddressCountry": "", "jobAddressPostal": ""},
+    ];
   dataLength = this.searchResults.length
+
+  constructor(
+    public jobsService: JobsService
+  ) {}
+
+  ngOnInit() {
+    this.jobsService.getJobs().subscribe(response: { jobs: Job[]; postCount: number }) => {
+
+    });
+  }
+
   openListing = (id: number) => {
 
   }
-}
-
-export class Listing {
-
-  constructor(
-    public id: number,
-    public jobTitle: string,
-    public skillName: string,
-    public jobImage: string,
-    public jobDescription: string,
-    public jobCategory: string,
-    public jobPostingDate: string
-  ) {  }
-
 }
